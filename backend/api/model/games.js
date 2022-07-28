9459166ad065bdecd16dd57b7a498fe3f329a329
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-28 10:28:44 +0200

    initial working API

---
+++ backend/api/model/games.js
@@ -0,0 +1,3 @@
+var games = [];
+
+module.exports = games
---
+++ backend/api/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+//env vars
+const PORT = process.env.PORT || 3000
+
+//lib
+const makeid = require('./lib/makeid')
+
+//imports
+const games = require('./model/games')
+const Game = require('../game/game')
+
+app.get('/api/game/create', (req,res)=>{
+    let newGame = {
+        created_at: new Date().toDateString(),
+        created_from: req.ip,
+        invite_code: makeid(10),
+        game: new Game()
+    }
+
+    games.push(newGame)
+    res.status(200).json(newGame)
+})
+
+app.listen(PORT, ()=>{console.log('I AM UP, listening on port', PORT)})
