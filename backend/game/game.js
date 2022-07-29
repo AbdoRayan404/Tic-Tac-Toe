3237a624806fa3e6bed448cfffb3eaef14754ac0
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-29 07:00:35 +0200

    changing addPlayer to signPlayer, playerNum -> playerID so instead of adding player using the player num and all of these mess it was more effecient to use playerID.

--- backend/game/game.js
+++ backend/game/game.js
@@ -18,6 +18,10 @@ class game {
         return this._status
     }
 
+    get gameID(){
+        return this._gameID
+    }
+
     state(arg){
         if(!arg) return this._state; //this will return state in rows
         else if(arg === "columns") return [[this._state[0][0], this._state[1][0], this._state[2][0]], [this._state[0][1], this._state[1][1], this._state[2][1]], [this._state[0][2], this._state[1][2], this._state[2][2]]]
@@ -25,47 +29,51 @@ class game {
         else throw new Error('unknown argument in state inspect')
     }
 
-    setPlayer(playerNum, id){
-        if(playerNum > 2 || playerNum < 1) throw new Error('invalid player number')
-        if(this[`_player${playerNum}`].id) throw new Error('player already has an id')
-
-        this[`_player${playerNum}`].id = id;
-        if(this._player1.id && this._player2.id){
-            this._status = 'ongoing'
-            this._player1.symbol = "X"
-            this._player2.symbol = "O"
+    signPlayer(id){
+        if(this._player1.id === null){
+            this._player1.id = id;
+        }else if(this._player2.id === null){
+            this._player2.id = id;
         }
 
-        return this[`_player${playerNum}`]
-    }
-
-    play(playerNum, x,y){
-        if(this._status != 'ongoing') throw new Error("this game is not started yet, or done")
-        if(playerNum > 2 || playerNum < 1) throw new Error('invalid player number')
-        if(this[`_player${playerNum}`].id == this._lastPlayFrom) throw new Error('player cannot play twice in a row')
-        if(this._state[x][y] != " ") return "used";
-        this._state[x][y] = this[`_player${playerNum}`].symbol;
-        this._lastPlayFrom = this[`_player${playerNum}`].id;
-
-        this.check(playerNum)
+        if(this._player1.id && this._player2.id){
+            this._status = 'ongoing'
+            this._player1.symbol = "X";
+            this._player2.symbol = "O";
+        }
 
         return this._status
     }
 
-    check(playerNum){
+    play(id, x,y){
+        if(this._status != 'ongoing') throw new Error("this game is not started yet, or done")
+        let player = this._player1.id == id ? this._player1 : this._player2
+        if(id == this._lastPlayFrom) throw new Error('player cannot play twice in a row')
+        if(this._state[x][y] != " ") return "used";
+        this._state[x][y] = player.symbol;
+        this._lastPlayFrom = id;
+
+        this.check(id)
+
+        return this._status
+    }
+
+    check(id){
+        let player = this._player1.id == id ? this._player1 : this._player2
+
         let rows = this.state();
         for(let i = 0; i < rows.length; i++){
-            if(new RegExp(`^${this[`_player${playerNum}`].symbol}{3,}$`).test(rows[i].join('')) === true) return this._status = ['win', this[`_player${playerNum}`].id]
+            if(new RegExp(`^${player.symbol}{3,}$`).test(rows[i].join('')) === true) return this._status = ['win', id]
         }
 
         let columns = this.state('columns');
         for(let i = 0; i < columns.length; i++){
-            if(new RegExp(`^${this[`_player${playerNum}`].symbol}{3,}$`).test(columns[i].join('')) === true) return this._status = ['win', this[`_player${playerNum}`].id]
+            if(new RegExp(`^${player.symbol}{3,}$`).test(columns[i].join('')) === true) return this._status = ['win', id]
         }
 
         let diagonals = this.state('diagonals');
         for(let i = 0; i < diagonals.length; i++){
-            if(new RegExp(`^${this[`_player${playerNum}`].symbol}{3,}$`).test(diagonals[i].join('')) === true) return this._status = ['win', this[`_player${playerNum}`].id]
+            if(new RegExp(`^${player.symbol}{3,}$`).test(diagonals[i].join('')) === true) return this._status = ['win', id]
         }
 
         let hasEmptyField = false;
