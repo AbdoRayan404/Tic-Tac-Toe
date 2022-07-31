a0ff2ecbf3d414df9274fb30b4295d6c3037fe80
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-31 23:48:58 +0200

    changing the way events listener are added

--- backend/api/wss/server.js
+++ backend/api/wss/server.js
@@ -5,7 +5,19 @@ const wss = new WebSocketServer({"noServer":true})
 
 //handlers
 const connectionHandler = require('./handlers/connectionHandler')
+const messageHandler = require('./handlers/messageHandler')
 
-wss.on('connection', connectionHandler)
+//sockets
+const webSockets = require('./model/websockets')
+
+wss.on('connection', (ws, request)=>{
+    connectionHandler(ws, request)
+    
+    if(ws.gameID){ //this means the user have joined a game.
+        ws.on('message', (data)=>{
+            messageHandler(ws, data)
+        })
+    }
+})
 
 module.exports = wss;
