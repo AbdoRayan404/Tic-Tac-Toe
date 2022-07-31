25898a32e6d5cc9aea5427ae99e99a9f6a58d472
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-31 23:46:28 +0200

    more handlers these handlers are just to not duplicate code, and make it into one function that can be called more than once

---
+++ backend/api/wss/handlers/gameUpdate.js
@@ -0,0 +1,18 @@
+const webSockets = require('../model/websockets')
+const games = require('../../model/games')
+
+module.exports = (gameID)=>{
+    for(let i = 0; i < games.length; i++){
+        if(games[i].game.gameID === gameID){
+            let p1 = games[i].game._player1.id;
+            let p2 = games[i].game._player2.id;
+            for(let j = 0; j < webSockets.length; j++){
+                if(webSockets[j].playerID === p1){
+                    webSockets[j].send(JSON.stringify({"status":games[i].game.status, "state":games[i].game.state()}))
+                }else if(webSockets[j].playerID === p2){
+                    webSockets[j].send(JSON.stringify({"status":games[i].game.status, "state":games[i].game.state()}))
+                }
+            }
+        }
+    }
+}
