864913f309affe077da1be1de0b04525fdc3be50
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-08-01 07:35:50 +0200

    working frontend, with landing page this is a working frontend with a light server to serve it's files. it will still have more dynamic stuff added into it, i only added the landing page, which is 100% working it is simple page to create new game or join a game already, also it is already merged a little with the backend.

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logitHTTP = require('logit-http');
+
+const app = express()
+
+//middlewares
+app.use(logitHTTP)
+app.use(express.json())
+app.use(express.static('views/src'))
+
+//VARS
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/landing.html')
+})
+
+app.listen(PORT, ()=> console.log('This bad boy is up, at port', PORT))
---
+++ frontend/views/src/landing.js
@@ -0,0 +1,33 @@
+//HELLO there, open source code reader, just wanted to say that i like you brotha.
+//add me in discord, we might have small talk about coding Nameless#8663
+
+function create(){
+    fetch('http://localhost:3000/api/game/create')
+        .then((response)=>{
+            if(response.status == 200){
+                return response.json()
+            }
+        })
+        .then((data)=>{
+            window.location.href = 'game/' + data.invite;
+        })
+}
+
+function join(){
+    let invite = document.querySelector('#game #invite_code')
+    let error = document.querySelector('#game #error')
+
+    //check the invite code first
+    fetch('http://localhost:3000/api/game/inspect/' + invite.value)
+        .then((response)=>{
+            if(response.status == 200){
+                window.location.href = 'game/' + invite.value
+            }else if(response.status == 404){
+                error.style.display = 'block';
+                error.innerHTML = 'game wasnt found'
+            }else if(response.status == 423){
+                error.style.display = 'block'
+                error.innerHTML = 'game is ongoing, you cannot join'
+            }
+        })
+}
