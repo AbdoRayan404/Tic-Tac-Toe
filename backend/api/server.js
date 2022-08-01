5015d7a358dd90ed97566e05b46bffc7bee7f624
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-08-01 07:32:13 +0200

    adding CORS this is not really the best practice i know, having an open CORS for everyone but this is temporary in this development stage till i merge both frontend and backend into one server so i will no longer need CORS to be open for anyone

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logit = require('logit-http');
 const app = express();
+const cors = require('cors');
 
 //env vars
 const PORT = process.env.PORT || 3000
@@ -12,12 +13,15 @@ const server = app.listen(PORT, ()=>{console.log('HTTP server is up, listening o
 const rateLimiter = require('./middlewares/handlers/rateLimiter')
 const upgradeHandler = require('./middlewares/handlers/upgradeHanlder')
 const gameCreate = require('./middlewares/routes/gameCreate')
+const gameInspect = require('./middlewares/routes/gameInspect')
 
 //HTTP -> WS upgrade handling
 server.on('upgrade', upgradeHandler)
 
 //middlewares
+app.use(cors())
 app.use(logit)
 app.use(rateLimiter)
 
 app.get('/api/game/create', gameCreate)
+app.get('/api/game/inspect/:invite', gameInspect)
